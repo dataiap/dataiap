f99331c696d0fe29b3f555973e4c04d988b41913
Adam Marcus <marcua@marcua.net>
2011-12-17 15:15:09 -0500

    removed all dependencies on R or rpy2.  Use scipy and welchttest.py

--- datasets/pres_campaign_2012/ttest.py
+++ datasets/pres_campaign_2012/ttest.py
@@ -1,14 +1,11 @@
 import csv,sys,datetime,collections
-import itertools
-import matplotlib.pyplot as plt
-import rpy2.robjects as R
 import numpy
-import rpy_helper
+import scipy.stats
+import welchttest
 
 
-# requires R
-# requires rpy2
 # to install matplot lib, see https://jholewinski.wordpress.com/2011/07/21/installing-matplotlib-on-os-x-10-7-with-homebrew/
+
 reader = csv.DictReader(open(sys.argv[1], 'r'))
 idx = 0
 
@@ -22,9 +19,28 @@ for row in reader:
 obama = candtomoney["Obama Barack"]
 mccain = candtomoney["McCain John S"]
 
+# certainly the means look different...
 print "Obama mean, stdev", numpy.mean(obama), numpy.std(obama)
 print "McCain mean, stdev", numpy.mean(mccain), numpy.std(mccain)
 
-args = {'var.equal': False, 'paired': False}
-result = rpy_helper.pify(R.r["t.test"](R.FloatVector(obama), R.IntVector(mccain), **args))
-print result['p.value'], result['statistic'], result['method'], result.keys()
+# running a ttest of independent samples suggests that they aren't the same mean
+print "ttest, equal variances", scipy.stats.ttest_ind(obama, mccain)
+
+# small white lie: there is no reason to believe that these two samples have
+# equal variance, so let's use a welch test.
+print "welch", welchttest.welchs_approximate_ttest_arr(obama, mccain)
+
+# but we've been lying to you.  you should only run ttests on normal data, so run
+# the shapiro-wilk test of normalcy
+print "obama shapiro", scipy.stats.shapiro(obama)
+print "mccain shapiro", scipy.stats.shapiro(mccain)
+
+# ooops...we have to reject the null hypothesis: it's very unlikely these two
+# are normally distributed.  it's actually not that bad for a ttest, but just to
+# be sure, let's run a non-parametric test called the Mann-Whitney U test.
+print "mann-whitney U", scipy.stats.mannwhitneyu(obama, mccain)
+
+# cool!  the p-value is 0.  So if our alpha was .05 or .01, we'd still be below
+# it.  it's unlikely these two are from the same distribution, and thus we
+# can safely reject the null hypothesis that they have the same mean.  Obama's
+# donations were really smaller than McCain's!
