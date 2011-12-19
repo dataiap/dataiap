c8f62d3d3a1e4b1a4eec14b19f26497656574dd5
eugene wu <sirrice@gmail.com>
2011-12-19 18:02:59 -0500

    added library for plotting US map data, commented vis code

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,8 @@
-import random
+import random, json
 import numpy as np
 import matplotlib.pyplot as plt
+from util import *
+from collections import defaultdict
 from matplotlib.patches import Rectangle
 random.seed(0)
 
@@ -22,7 +24,7 @@ ys2 = range(N)
 # setup the Figure
 # figsize - (width, height) in inches
 # 
-fig = plt.figure(figsize=(50, 10))
+fig = plt.figure(figsize=(50, 30))
 
 #
 # the figure is the container that our plots will be drawn in
@@ -59,19 +61,21 @@ fig = plt.figure(figsize=(50, 10))
 # - subplot.legend() - add a legend
 # - subplot.set_title()
 # - subplot.set_xlabel()
-# - subplot.set_ylabel()
 # - subplot.set_xticks()
 # - subplot.set_xticklabels()
+# - subplot.set_xscale() - 'linear'/'log'/'symlog'
+# - subplot.set_ylabel()
 # - subplot.set_yticks()
 # - subplot.set_yticklabels()
+# - subplot.set_yscale()
 #
+
+
+
+
+# bar graphs
+# http://matplotlib.sourceforge.net/api/axes_api.html#matplotlib.axes.Axes.bar
 #
-
-
-
-
-#
-# bar graphs http://matplotlib.sourceforge.net/api/axes_api.html#matplotlib.axes.Axes.bar
 # left  - specifies the x coord of the left side of the bar.  The units are meaningless
 #         because matplotlib internall computes the pixels to fit the subplot width
 # width - used to specify the width of a bar, and the offset of the second bar. its value
@@ -80,9 +84,10 @@ fig = plt.figure(figsize=(50, 10))
 left = np.arange(len(xs))  * 2.0
 width = 0.5
 
-# note: add_subplot(xyz) means the figure is an x rows by y cols grid
+# note: add_subplot(x,y,z) means the figure is an x rows by y cols grid
 #       and z is the subfigure within the grid (row first)
-subplot = fig.add_subplot(221)
+# note: add_subplot(xyz) is a shorthand when x,y,z are single digits
+subplot = fig.add_subplot(321)
 subplot.bar(left, ys, width, color=colors[0], linewidth=0)
 subplot.bar(left+width, ys2, width, color=colors[1], bottom=ys, linewidth=0)
 subplot.set_title("Bargraph Example")
@@ -90,19 +95,24 @@ subplot.set_title("Bargraph Example")
 
 
 # line graphs
-#
-subplot = fig.add_subplot(222)
-subplot.plot(xs, ys, xs, ys)
+# http://matplotlib.sourceforge.net/api/axes_api.html#matplotlib.axes.Axes.plot
+# 
+subplot = fig.add_subplot(322)
+# shorthand for:
+#   subplot.plot(xs, ys)
+#   subplot.plot(xs, ys2)
+subplot.plot(xs, ys, xs, ys2)
 subplot.set_title("Line graph/plot Example")
 
 
 # boxplot graphs
+# http://matplotlib.sourceforge.net/api/axes_api.html#matplotlib.axes.Axes.plot
 #
 # each boxdata contains the set of points to generate a single box
 # the boxplot() method takes a list of boxdatas, and generates a box for each
 # set of points.
 #
-subplot = fig.add_subplot(223)
+subplot = fig.add_subplot(323)
 boxdata1 = [random.randint(0, 20) for i in xrange(10)]
 boxdata2 = [random.randint(20,40) for i in xrange(10)]
 boxdata3 = [random.randint(40,60) for i in xrange(10)]
@@ -112,11 +122,41 @@ subplot.set_title("Boxplot Example")
 
 
 # Scatterplot Graph
+# http://matplotlib.sourceforge.net/api/axes_api.html#matplotlib.axes.Axes.plot
 #
-subplot = fig.add_subplot(224)
+# 
+subplot = fig.add_subplot(324)
 subplot.scatter(xs, ys, color=colors[0])
-subplot.scatter(xs, ys2,color=colors[1],s=ys2) # size varies with y
+# size (s keyword arg) varies with ys2
+subplot.scatter(xs, ys2,color=colors[1],s=ys2)
 subplot.set_title("Scatterplot Example")
+
+#########################
+# Geographical Graph
+#########################
+
+
+# Plot by County
+#
+subplot = fig.add_subplot(325)
+data = json.load(file('../datasets/geo/id-counties.json'))
+for fips in data:
+    draw_county(subplot, fips)
+subplot.set_title("Counties Example")
+subplot.set_xlim(-200, -50)
+subplot.set_ylim(15, 80)
+
+# State Graph
+#
+subplot = fig.add_subplot(326)
+data = json.load(file('../datasets/geo/id-states.json'))
+for state in data:
+    draw_state(subplot, state)
+subplot.set_title("States Example")
+subplot.set_xlim(-200, -50)
+subplot.set_ylim(15, 80)
+
+
 
 
 plt.savefig('/tmp/test.png', format='png')
