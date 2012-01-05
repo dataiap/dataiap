cf9f6710b16f04f6bedf0586ba3d299fe7aa5a3f
Adam Marcus <marcua@marcua.net>
2012-01-05 17:27:29 -0500

    cleanup

--- resources/create_iam_accounts.py
+++ resources/create_iam_accounts.py
@@ -12,9 +12,10 @@ iam = boto.connect_iam(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 print "username,password,access_key_id,secret_access_id"
 for i in range(num_accounts):
-    username = "user_%s_%d" % (prefix, i)
+    username = "dataiap_%s_%d" % (prefix, i)
     password = base64.b64encode(os.urandom(100))[:10]
     user = iam.create_user(username)
+    iam.add_user_to_group("dataiap_students", username)
     keys = iam.create_access_key(username)
     iam.create_login_profile(username, password)
     print "%s,%s,%s,%s" % (username, password, keys.access_key_id, keys.secret_access_key)
