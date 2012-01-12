efdd43cbcb412700eb784ecf00d84700139cf905
Adam Marcus <marcua@marcua.net>
2012-01-12 10:48:51 -0500

    day 4 updates

---
+++ day4/get_terms.py
@@ -0,0 +1,17 @@
+import re
+
+STOPWORDS = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'])
+
+def get_terms(s):
+    s = s.lower()
+    terms = s.split()
+    terms = map(lambda term: term.replace("'s",'').replace("'", '').replace(".", "").replace(",", ""), terms)
+    terms = filter(lambda term: len(term) > 3, terms)
+    terms = filter(lambda term: term not in STOPWORDS, terms)
+    terms = filter(lambda term: re.match("^[a-zA-Z]+[a-zA-Z\'\-]*[a-zA-Z]+$", term), terms)
+    return terms
+
+if __name__ == "__main__":
+    example = "cats with hats are awesome-town, and 'and -eat food- lik*e most of us as long as the food is tasty."
+    print example
+    print get_terms(example)
